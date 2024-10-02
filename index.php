c8d290598d46e9e17d30aa6cd47ec1e9ab323fc0
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-02 15:37:06 +0100

    functions

--- index.php
+++ index.php
@@ -1,78 +1,47 @@
 <?php
 
-
-# Enable PHP error reporting for debugging
-error_reporting(E_ALL); // Report all types of errors
-ini_set('display_errors', 1); // Display errors on the screen
-
-include 'includes/db_connect.php';
+    # Function is a block of reusable code that performs a specific task
 
 
-# DATABASE
-
-
-# Sample articles data stored in the system (as in a database table)
-// $articles = [
-//     [
-//         'title' => 'First post',
-//         'content' => 'This is the first of many posts'
-//     ],
-//     [
-//         'title' => 'Another post',
-//         'content' => 'Yet another fascinating posts...'
-//     ],
-//     [
-//         'title' => 'Read this!',
-//         'content' => 'You must read this now, it\'s essential reading!'
-//     ],
-//     [
-//         'title' => 'The latest news',
-//         'content' => 'Here is the latest new, read it now!'
-//     ]
-// ];
-
-
-// print_r($articles);
-
-
-$sql = "SELECT * FROM article ORDER BY published_at";
-
-$result = mysqli_query($conn, $sql);
-
-if (!$result) {
-    // Query failed
-    echo "Query failed: " . mysqli_error($conn);
-} elseif (mysqli_num_rows($result) == 0) {
-    // Query successful but no rows found
-    echo "Nothing to fetch as table is empty!";
+// Function with no parameters that prints a message
+function sendMessage(){
+    echo "I was told to send a message </br>";
 }
 
+sendMessage();   // Calling the function
 
-$articles = mysqli_fetch_all($result, MYSQLI_ASSOC); // FETCH RESULT AS ASSOCIATE ARRAY
-
-
-# Close the database connection after fetching the data
-mysqli_close($conn);
-
-?>
+echo "</br>";
 
 
-<?php require 'includes/header.php' ?>
 
-    <?php if(empty($articles)) : ?>
-    <p>No article found.</p>
-    <?php else: ?>
-        <ul>
-            <?php foreach($articles as $article) : ?>
-                <li>
-                    <article>
-                        <h2><a href="article.php?id=<?= $article['id']; ?>"><?= $article['title']; ?></a></h2>
-                        <p><?= $article['content']; ?></p>
+// Function that takes one parameter and prints it
+function callSomeone($name){
+    echo "I was told to call $name </br>";
+}
 
-                    </article>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+callSomeone('Dave');   // Calling the function with an argument
 
-<?php require 'includes/footer.php' ?>
+echo "</br>";
+
+
+
+// Function that takes two parameters and prints a message
+function callPerson($age, $gender = 'Male'){  // Default value for $gender
+    $name = 'Joshua';  // Local variable
+    echo "His name is $name, he's $age years old and his gender is $gender. </br>";
+}
+
+callPerson(25);   // Calling the function with one argument (gender will use the default value 'Male')
+
+echo "</br>";
+
+
+
+// Function that returns a value (output can be used outside the function)
+function returnValue($language, $role){
+    $level = 'Junior';  // Local variable
+    echo "He is a $level $language $role developer. </br>";
+}
+
+echo returnValue('PHP', 'Backend');   // Calling the function and displaying the result
+
