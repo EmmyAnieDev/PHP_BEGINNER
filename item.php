916b69e3946125f78c895cfcca31c5afb9396003
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-03 23:20:45 +0100

    public and private visibilty for properties and methods

--- item.php
+++ item.php
@@ -2,17 +2,25 @@
 
 class Item {
 
-    public $name;
+    public $name;    ///  public properties can be used or accesed outside the class unlike private
     public $description = 'This is the default';  // default value to the properties
+    private $year;    // since it private it can only be used inside this class
+
 
     // When an object is created, initialize the object's properties
-    public function __construct($name, $description) {
+    public function __construct($name, $description, $year) {
         $this->name = $name;
         $this->description = $description;
+        $this->year = $year;
     }
 
+    // same as properties, public method can be accessible outisde the class.
+    public function sayHello() {
+        return 'Hello ' . $this->name . ' year is ' . $this->year;
+    }
 
-    function sayHello() {
-        return 'Hello ' . $this->name;
+    // same as properties, private method cannot be accessible outisde the class.
+    private function yearDifferences() {
+        return 2024 - $this->year;
     }
 }
--- object.php
+++ object.php
@@ -1,14 +1,19 @@
 <?php
 
+error_reporting(E_ALL); 
+ini_set('display_errors', 1); 
+
 require 'item.php';
 
 // creating an object from the class
-$my_item = new Item('Huge', 'it\'s a big item');
-$my_item2 = new Item('Small', 'it\'s a small item');
+$my_item = new Item('Huge', 'it\'s a big item', 2016);
+$my_item2 = new Item('Small', 'it\'s a small item', 2003);
 
 
 echo $my_item->name . '<br/>';
 echo $my_item->sayHello() . '<br/>';
+//echo $my_item->year . '<br/>';  // this will throw an erorr because we can't access private property outside the class
+//echo $my_item->yearDifferences() . '<br/>';   // this will also throw an erorr because we can't access private methods outside the class
 
 echo '<br/>';
 
