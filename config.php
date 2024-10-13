ab3c4cd889bfa0886936d043f9eedf434c1a6607
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-13 12:31:34 +0100

    display Errors and Exception details depending on environment

--- config.php
+++ config.php
@@ -9,3 +9,8 @@ define('DB_PASS', 'test1234');
 define ('SMTP_HOST', 'smtp.gmail.com');
 define ('SMTP_USER', 'example@gmail.com');
 define ('SMTP_PASS', '1234567890');
+
+// Show detailed error information based on the application's environment.
+// Set to 'true' to display detailed error messages in development.
+// Set to 'false' to hide error details in production for security reasons.
+define('SHOW_ERROR_DETAILS', false);
